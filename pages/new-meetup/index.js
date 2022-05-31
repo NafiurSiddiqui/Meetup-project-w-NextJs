c7e0ea2ba698f88fcc956101b95d12d8cbbe31c8
saejim <saejim91@gmail.com>
2022-06-01 02:48:34 +0600

    MongoDB connected successfully

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,16 +1,33 @@
 //our-domain/meet-up
-
-import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from 'next/router';
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-	const addMeetUpHandler = (enteredMeetupData) => {
-		console.log(enteredMeetupData);
-	};
+	const router = useRouter();
+	async function addMeetupHandler(enteredMeetupData) {
+		//send data to DB
+		const response = await fetch('/api/new-meetup', {
+			method: 'POST',
+			body: JSON.stringify(enteredMeetupData),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
 
-	return <NewMeetupForm onAddMeetup={addMeetUpHandler} />;
+		const data = await response.json();
+
+		console.log(data);
+
+		//To navigate user away to the home upon submission
+		router.push('/');
+	}
+
+	return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
 export default NewMeetupPage;
 
 /**
  * we DO NOT need the DATA here, hence we will not call the @getStaticProp
+ *
+ * @fetch - since this is inside our own server, no need to use a third or whole domain to another API, therefore, we used the absolute path.
  */
